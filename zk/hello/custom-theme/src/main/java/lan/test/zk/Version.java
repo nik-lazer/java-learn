c5379eef3bc193220e2dcd752c2ca1e2c8751ccd
nik-lazer <nick.lazarev@gmail.com>
2014-05-20 12:45:18 +0400

    Added custom theme, web-part moved to divided module

---
+++ zk/hello/custom-theme/src/main/java/lan/test/zk/Version.java
@@ -0,0 +1,7 @@
+package lan.test.zk;
+
+public class Version {
+	/** Returns the version UID.
+	 */
+	public static final String UID = "0.0.1";
+}
---
+++ zk/hello/web/src/main/java/lan/test/zk/MyViewModel.java
@@ -0,0 +1,28 @@
+package lan.test.zk;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zul.theme.Themes;
+
+public class MyViewModel {
+
+	private int count;
+
+	@Init
+	public void init() {
+		String theme = Themes.getCurrentTheme();
+		count = 100;
+	}
+
+	@Command
+	@NotifyChange("count")
+	public void cmd() {
+		++count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+}
