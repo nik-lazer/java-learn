527843f884a14095c03b5c31f5950acb2544a142
lazarev.nikolay <lazarev.nikolay@otr.ru>
2014-05-28 10:55:53 +0400

    Эксперименты с dsp для тем

---
+++ zk/hello/web/src/main/java/lan/test/zk/CustomThemeProvider.java
@@ -0,0 +1,38 @@
+package lan.test.zk;
+
+import org.zkoss.web.fn.ServletFns;
+import org.zkoss.zk.ui.Execution;
+import org.zkoss.zk.ui.util.ThemeProvider;
+import org.zkoss.zul.theme.Themes;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * TODO: comment
+ * @author lazarev_nv 23.05.2014   16:37
+ */
+public class CustomThemeProvider implements ThemeProvider {
+	public Collection<Object> getThemeURIs(Execution execution, List<Object> objects) {
+		if ("custom".equals(Themes.getCurrentTheme())) {
+			objects.add("~./custom/custom.css.dsp");
+		}
+		return objects;
+	}
+
+	public int getWCSCacheControl(Execution execution, String s) {
+		return -1;
+	}
+
+	public String beforeWCS(Execution execution, String s) {
+		return s;
+	}
+
+	public String beforeWidgetCSS(Execution execution, String uri) {
+		if (uri.startsWith("~./zul/css/") ||
+		    uri.startsWith("~./js/zul/")) {
+			uri = ServletFns.resolveThemeURL(uri);
+		}
+		return uri;
+	}
+}
