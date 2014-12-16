86d4f015f894d1a7a19a4d12506aeb067c32ac76
nik-lazer <nick.lazarev@gmail.com>
2014-12-16 13:02:51 +0300

    Samples of array declarations, initializators and clone sample

---
+++ java-learn/src/main/java/lan/learn/basic/ArraysSample.java
@@ -0,0 +1,26 @@
+package lan.learn.basic;
+
+/**
+ * Arrays declaration
+ * @author lazarev_nv  16.12.2014   11:47
+ */
+public class ArraysSample {
+	int a[];
+	int[] a1;
+	int[] a2[];
+	Object p, p1[], p2[][];
+
+	public static void main(String args[]) {
+		int i[]=new int[5];
+		int i1[]={1, 3, 5}; // initializator
+		String[] arr = new String[]{"firstName", "lastName"};
+		testClone();
+	}
+
+	public static void testClone() {
+		int a[][] = {{1, 2}, {3}};
+		int b[][] = (int[][]) a.clone();
+		a[0] = new int[]{0};
+		System.out.println(b[0][0]);
+	}
+}
