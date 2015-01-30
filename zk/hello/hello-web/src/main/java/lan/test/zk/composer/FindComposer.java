9c9339cf11f24be854ac05b25a6544354b086b75
nik-lazer <nick.lazarev@gmail.com>
2015-01-30 15:10:02 +0300

    encodeThemeURL in zul checked

--- zk/hello/hello-web/src/main/java/lan/test/zk/composer/FindComposer.java
+++ zk/hello/hello-web/src/main/java/lan/test/zk/composer/FindComposer.java
@@ -1,7 +1,9 @@
 package lan.test.zk.composer;
 
+import org.zkoss.web.fn.ServletFns;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Button;
 import org.zkoss.zul.Window;
 
 /**
@@ -9,9 +11,18 @@ import org.zkoss.zul.Window;
  * @author nik-lazer  19.12.2014   13:21
  */
 public class FindComposer extends SelectorComposer<Window> {
+	@Wire
+	private Button findButton;
+	@Wire
+	private Button findNextButton;
+	@Wire
+	private Button cancelButton;
 
 	@Override
 	public void doAfterCompose(Window comp) throws Exception {
 		super.doAfterCompose(comp);
+		findButton.setImage(ServletFns.resolveThemeURL("~./find.png"));
+		findNextButton.setImage(ServletFns.resolveThemeURL("~./find_next.png"));
+		cancelButton.setImage(ServletFns.resolveThemeURL("~./exit.png"));
 	}
 }
