95e5ad57a9da0f85054f82b400846741e7aaee31
nik-lazer <nick.lazarev@gmail.com>
2015-01-30 17:59:20 +0300

    component ExampleButton started

---
+++ zk/hello/hello-web/src/main/webapp/js/lan/test/ExtendButton.js
@@ -0,0 +1,18 @@
+lan.test.ExtendButton = zk.$extends(zul.wgt.Button, {
+	domContent_: function () {
+		var label = zUtl.encodeXML(this.getLabel()),
+				img = this.getImage(),
+				iconSclass = this.domIcon_();
+		if (!img && !iconSclass) return label;
+		label = "<div>"+ label + "</div>";
+
+		if (!img) img = iconSclass;
+		else
+			img = '<img class="' + this.$s('image') + '" src="' + img + '" />'
+					+ (iconSclass ? ' ' + iconSclass : '');
+		var space = "vertical" == this.getOrient() ? '<br/>': ' ';
+		return this.getDir() == 'reverse' ?
+		       label + space + img: img + space + label;
+	},
+
+});
