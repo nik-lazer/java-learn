a24b131d868940be876ae009514cba35298c2275
nik-lazer <nick.lazarev@gmail.com>
2015-02-06 15:30:08 +0300

    Popup widget was customized and got new styles

--- zk/hello/hello-web/src/main/java/lan/test/zk/au/PointerPopupAuResponse.java
+++ zk/hello/hello-web/src/main/java/lan/test/zk/au/PointerPopupAuResponse.java
@@ -12,7 +12,7 @@ public class PointerPopupAuResponse extends AuResponse {
 	public PointerPopupAuResponse(String title, String msg, String desc, String type, Page page, Component ref,
 	                      String position, int duration, boolean closable) {
 		super("showPointerPopup", new Object[] {
-				msg, type, page.getUuid(), ref, position, null, duration, closable });
+				title, msg, desc, type, page.getUuid(), ref, position, null, duration, closable });
 	}
 
 	/** Shows notification at given coordinate
@@ -20,6 +20,6 @@ public class PointerPopupAuResponse extends AuResponse {
 	public PointerPopupAuResponse(String title, String msg, String desc, String type, Page page, Component ref,
 	                      int x, int y, int duration, boolean closable) {
 		super("showPointerPopup", new Object[] {
-				msg, type, page.getUuid(), ref, null, new int[]{x, y}, duration, closable });
+				title, msg, desc, type, page.getUuid(), ref, null, new int[]{x, y}, duration, closable });
 	}
 }
--- zk/hello/hello-web/src/main/resources/web/js/lan/test/PointerPopupWindow.js
+++ zk/hello/hello-web/src/main/resources/web/js/lan/test/PointerPopupWindow.js
@@ -1,17 +1,32 @@
-zAu.cmd0.showPointerPopup = function (msg, type, pid, ref, pos, off, dur, closable) {
-	var notif = (zul && zul.wgt) ? zul.wgt.Notification : null;
+zAu.cmd0.showPointerPopup = function (title, msg, desc, type, pid, ref, pos, off, dur, closable) {
+	var notif = (lan && lan.test) ? lan.test.PointerPopupWindow : null;
 	if (notif) {
-		notif.show(msg, pid, {ref:ref, pos:pos, off:off, dur:dur, type:type, closable:closable});
+		notif.show(msg, pid, {ref:ref, pos:pos, off:off, dur:dur, type:type, closable:closable, title:title, desc:desc});
 	} else {
 
 		jq.alert(msg);
 	}
 };
 
+(function () {
+	var _iconMap = {
+		'warning': 'z-icon-exclamation-circle',
+		'info': 'z-icon-info-circle',
+		'error': 'z-icon-times-circle'
+	};
+	var _dirMap = {
+		'u': 'up',
+		'd': 'down',
+		'l': 'left',
+		'r': 'right'
+	};
+
 lan.test.PointerPopupWindow = zk.$extends(zul.wgt.Notification, {
 
 	$init: function (msg, opts) {
 		this.$supers(zul.wgt.Notification, '$init', arguments);
+		this._title = opts.title;
+		this._desc = opts.desc;
 		this._msg = msg;
 		this._type = opts.type;
 		this._ref = opts.ref;
@@ -24,12 +39,55 @@ lan.test.PointerPopupWindow = zk.$extends(zul.wgt.Notification, {
 		out.push('<div', this.domAttrs_(), '>');
 		if (this._ref)
 			out.push('<div id="', uuid, '-p" class="', this.$s('pointer'), '"></div>');
-		out.push('<i id="', uuid, '-icon" class="', icon, ' ', (_iconMap[this._type]), '"></i>');
-		out.push('<div id="', uuid, '-cave" class="', this.$s('content'), '">',
+		out.push('<div id="', uuid, '-cave" class="', this.$s('content'), '">');
+		out.push('<div id="', uuid, '-title-cave" class="', this.$s('title'), '">');
+		out.push('<div id="', uuid, '-title-image-cave" class="', this.$s(this._type +'-image'), '">&nbsp;</div>');
+		out.push('<div id="', uuid, '-title-text-cave" class="', this.$s('title-text'), '">',
 				this._msg, '</div>');
+		out.push('</div>');
+		out.push('<div id="', uuid, '-msg-cave" class="', this.$s('msg'), '">',
+				this._msg, '</div>');
+		out.push('<div id="', uuid, '-desc-cave" class="', this.$s('desc'), '">',
+				this._desc, '</div>');
+		out.push('</div>');
 		if (this._closable)
 			out.push('<div id="', uuid, '-cls" class="', this.$s('close'),
 					'"><i id="', uuid, '-clsIcon" class="', icon, ' z-icon-times"></i></div>');
 		out.push('</div>');
 	}
+}, {
+	show: function (msg, pid, opts) {
+		if (!opts)
+			opts = {};
+		var parent = zk.Widget.$(pid),
+				ref = opts.ref,
+				pos = opts.pos,
+				dur = opts.dur,
+				title = opts.title;
+				desc = opts.desc;
+				ntf = new lan.test.PointerPopupWindow(msg, opts),
+				off = opts.off;
+
+
+
+		if (!pos && !off)
+			pos = ref ? "end_center" : "middle_center";
+
+		if (!parent) {
+
+			parent = zk.Desktop.$().firstChild;
+		}
+		parent.appendChild(ntf);
+		ntf.open(ref, off, pos);
+
+
+		if (dur > 0)
+			setTimeout(function () {
+				if (ntf.desktop)
+					ntf.close();
+			}, dur);
+	}
+
 });
+
+})();
