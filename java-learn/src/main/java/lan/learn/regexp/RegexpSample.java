a70ab200878f1b35ec4872b9ab53fcc284f981f1
nik-lazer <nick.lazarev@gmail.com>
2015-10-04 20:52:46 +0300

    String.split with regexp

--- java-learn/src/main/java/lan/learn/regexp/RegexpSample.java
+++ java-learn/src/main/java/lan/learn/regexp/RegexpSample.java
@@ -8,6 +8,11 @@ import java.util.regex.Pattern;
  */
 public class RegexpSample {
 	public static void main(String[] args) {
+		matcherFind();
+		splitByRegexp();
+	}
+
+	public static void matcherFind() {
 		String patternString = ".ing";
 		String data = "ing ring trying running beings";
 		Pattern pattern = Pattern.compile(patternString);
@@ -16,6 +21,14 @@ public class RegexpSample {
 		while (matcher.find(i)) {
 			System.out.println("Finded: " + matcher.group() + "[" + matcher.start() + ", " + matcher.end() + "]");
 			i = matcher.end() + 1;
+		}
+	}
+
+	public static void splitByRegexp() {
+		String data = "3035551212,123 Main St.\tDenver,CO:50431";
+		String [] results = data.split("[;,:\\t]");
+		for(String result : results) {
+			System.out.println(result);
 		}
 	}
 }
