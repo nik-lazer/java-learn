ea1b5396561fc06fbff168e5d309c819f08845fe
nik-lazer <nick.lazarev@gmail.com>
2015-11-01 22:36:10 +0300

    File searching sample

--- training/basic/src/main/java/lan/training/nio2/FileWalk.java
+++ training/basic/src/main/java/lan/training/nio2/FileWalk.java
@@ -14,8 +14,18 @@ public class FileWalk {
 		Path path = new File(classLoader.getResource("nio2/walk").getFile()).toPath();
 
 		Path pathSource = Paths.get(path.toUri());
+		System.out.printf("Path: %s%n", path);
 		try {
+			System.out.println("Simple walk started");
 			Files.walkFileTree(pathSource, new MyFileVisitor());
+			System.out.println("Simple walk finished");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			System.out.println("Find walk started");
+			Files.walkFileTree(pathSource, new MyFileFindVisitor("glob:file*.txt"));
+			System.out.println("Find walk finished");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -34,6 +44,33 @@ public class FileWalk {
 		@Override
 		public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
 			System.out.println("----------Directory name:" + dir + " visited----------");
+			return FileVisitResult.CONTINUE;
+		}
+	}
+
+	static class MyFileFindVisitor extends SimpleFileVisitor<Path> {
+		private PathMatcher matcher;
+
+		public MyFileFindVisitor(String pattern) {
+			try {
+				matcher = FileSystems.getDefault().getPathMatcher(pattern);
+			} catch(IllegalArgumentException iae) {
+				System.err.println("Invalid pattern; did you forget to prefix \"glob:\"?(as in  glob:*.java)");
+				System.exit(1);
+			}
+
+		}
+		public FileVisitResult visitFile(Path path, BasicFileAttributes fileAttributes){
+			find(path);
+			return FileVisitResult.CONTINUE;
+		}
+		private void find(Path path) {
+			Path name = path.getFileName();
+			if(matcher.matches(name))
+				System.out.println("Matching file:" + path.getFileName());
+		}
+		public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes fileAttributes){
+			find(path);
 			return FileVisitResult.CONTINUE;
 		}
 	}
